0a8b3516bd38c624efec8818521d5040c9a112e7
likunshang <shanglihero@163.com>
2018-11-14 23:03:42 +0800

    nongshi page

--- app/src/main/java/com/today/todayfarm/pages/EditFarmThing/EditFarmthingGuangaiActivity.java
+++ app/src/main/java/com/today/todayfarm/pages/EditFarmThing/EditFarmthingGuangaiActivity.java
@@ -2,6 +2,7 @@ package com.today.todayfarm.pages.EditFarmThing;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.today.todayfarm.R;
@@ -17,6 +18,21 @@ public class EditFarmthingGuangaiActivity extends Activity {
 
     @BindView(R.id.edit)
     TextView edit;
+
+    @BindView(R.id.fieldname) TextView tvfieldname;
+    @BindView(R.id.cropInfo) TextView tvcropinfo;
+    @BindView(R.id.starttime) TextView tvstarttime;
+    @BindView(R.id.selectstarttime) TextView tvselectstarttime;
+    @BindView(R.id.waterpress)
+    EditText etwaterpress;
+    @BindView(R.id.epress) EditText etepress;
+    @BindView(R.id.devicespeed) EditText etdevicespeed;
+    @BindView(R.id.endtime) TextView tvendtime;
+    @BindView(R.id.selectendtime) TextView tvselectendtime;
+    @BindView(R.id.price) EditText etprice;
+    @BindView(R.id.beizhu) EditText etbeizhu;
+    @BindView(R.id.delete) TextView tvdelete;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
